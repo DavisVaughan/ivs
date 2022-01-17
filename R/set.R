cbc224ea8510110e26ed46af3ede5d5945bbc7f1
DavisVaughan <davis@rstudio.com>
2022-01-17 08:39:21 -0500

    Implement `iv_set_union()` and `iv_set_intersect()`

--- R/set.R
+++ R/set.R
@@ -1,13 +1,26 @@
-#' Complement
+#' Set theoretical operations
 #'
-#' `iv_set_complement()` computes the set theoretical complement of an interval
-#' vector. The "universe" to compute the complement over is considered to be
-#' the minimal and maximal values of `x`, but this can be adjusted with `lower`
-#' and `upper`.
+#' @description
+#' This family of functions treats ivs as sets. They always compute
+#' the [minimal][iv_union()] iv of each input and return a minimal iv.
+#'
+#' - `iv_set_complement()` takes the complement of a set of intervals. By
+#'   default, the minimum and maximum of the inputs define the bounds to take
+#'   the complement over, but this can be adjusted with `lower` and `upper`.
+#'   Missing intervals are always dropped in the complement.
+#'
+#' - `iv_set_union()` takes the union of two sets of intervals. It is equivalent
+#'   to combining the two vectors together and then calling `iv_union()`.
+#'
+#' - `iv_set_intersect()` takes the intersection of two sets of intervals.
 #'
 #' @inheritParams rlang::args_dots_empty
 #'
 #' @param x `[iv]`
+#'
+#'   An interval vector.
+#'
+#' @param y `[iv]`
 #'
 #'   An interval vector.
 #'
@@ -17,20 +30,52 @@
 #'   have the same type as the element type of the interval vector. It is
 #'   often useful to expand the universe to, say, `-Inf` to `Inf`.
 #'
-#' @return An object with the same type as `x` representing its complement.
+#' @return
+#' For `iv_set_complement()`, a vector of the same type as `x` containing the
+#' complement.
 #'
-#' @export
+#' For all other set operations, a vector of the same type as the common type of
+#' `x` and `y` containing the result.
+#'
+#' @name iv-sets
+#'
 #' @examples
-#' x <- new_iv(c(10, 0, 3, -5), c(12, 5, 6, -2))
+#' x <- iv_pairs(
+#'   c(10, 12),
+#'   c(0, 5),
+#'   c(NA, NA),
+#'   c(3, 6),
+#'   c(-5, -2),
+#'   c(NA, NA)
+#' )
 #' x
 #'
+#' y <- iv_pairs(
+#'   c(2, 7),
+#'   c(NA, NA),
+#'   c(-3, -1),
+#'   c(14, 15)
+#' )
+#' y
+#'
 #' # Complement contains any values from `[-5, 12)` that aren't represented
-#' # in these intervals
+#' # in these intervals. Missing intervals are dropped.
 #' iv_set_complement(x)
 #'
 #' # Expand out the "universe" of possible values
 #' iv_set_complement(x, lower = -Inf)
 #' iv_set_complement(x, lower = -Inf, upper = Inf)
+#'
+#' # Union returns the minimal iv containing all of the information from
+#' # both interval vectors
+#' iv_set_union(x, y)
+#'
+#' # Minimal iv representing the set intersection
+#' iv_set_intersect(x, y)
+NULL
+
+#' @rdname iv-sets
+#' @export
 iv_set_complement <- function(x, ..., lower = NULL, upper = NULL) {
   proxy <- iv_proxy(x)
 
@@ -46,6 +91,73 @@ iv_set_complement <- function(x, ..., lower = NULL, upper = NULL) {
   )
 
   out <- new_iv(out$start, out$end)
+  out <- iv_restore(out, x)
+
+  out
+}
+
+#' @rdname iv-sets
+#' @export
+iv_set_union <- function(x, y) {
+  out <- vec_c(x, y)
+  iv_union(out)
+}
+
+#' @rdname iv-sets
+#' @export
+iv_set_intersect <- function(x, y) {
+  args <- vec_cast_common(x = x, y = y)
+  x <- args[[1]]
+  y <- args[[2]]
+
+  x_proxy <- iv_proxy(x)
+  y_proxy <- iv_proxy(y)
+
+  if (vec_size(x_proxy) == 0L) {
+    return(vec_ptype(x))
+  }
+  if (vec_size(y_proxy) == 0L) {
+    return(vec_ptype(x))
+  }
+
+  x_missing <- vec_equal_na(x_proxy)
+  y_missing <- vec_equal_na(y_proxy)
+
+  if (all(x_missing)) {
+    if (any(y_missing)) {
+      return(vec_init(x))
+    } else {
+      return(vec_ptype(x))
+    }
+  }
+  if (all(y_missing)) {
+    if (any(x_missing)) {
+      return(vec_init(x))
+    } else {
+      return(vec_ptype(x))
+    }
+  }
+
+  lower <- min(
+    min(field_start(x_proxy), na.rm = TRUE),
+    min(field_start(y_proxy), na.rm = TRUE)
+  )
+  upper <- max(
+    max(field_end(x_proxy), na.rm = TRUE),
+    max(field_end(y_proxy), na.rm = TRUE)
+  )
+
+  x_c <- iv_set_complement(x_proxy, lower = lower, upper = upper)
+  y_c <- iv_set_complement(y_proxy, lower = lower, upper = upper)
+
+  u <- iv_set_union(x_c, y_c)
+
+  out <- iv_set_complement(u, lower = lower, upper = upper)
+
+  if (any(x_missing) && any(y_missing)) {
+    out <- vec_c(out, vec_init(out))
+  }
+
   out <- iv_restore(out, x)
 
   out
--- tests/testthat/test-set.R
+++ tests/testthat/test-set.R
@@ -22,3 +22,139 @@ test_that("complement is generic", {
     new_nested_integer_iv(new_iv(3L, 5L))
   )
 })
+
+# ------------------------------------------------------------------------------
+# iv_set_union()
+
+test_that("set union doesn't keep abutting separate", {
+  x <- iv(start = c(1L, 2L), end = c(2L, 3L))
+  y <- iv(start = 5L, end = 6L)
+
+  expect_identical(
+    iv_set_union(x, y),
+    iv(start = c(1L, 5L), end = c(3L, 6L))
+  )
+})
+
+test_that("union treats intervals as half open `[a, b)`", {
+  x <- iv(start = 1L, end = 2L)
+  y <- iv(start = 3L, end = 5L)
+
+  expect_identical(
+    iv_set_union(x, y),
+    iv(start = c(1L, 3L), end = c(2L, 5L))
+  )
+})
+
+test_that("union keeps a single missing interval", {
+  x <- iv(c(1, NA, NA), c(2, NA, NA))
+  y <- iv(2, 3)
+
+  expect_identical(
+    iv_set_union(x, y),
+    iv_pairs(c(1, 3), c(NA, NA))
+  )
+  expect_identical(
+    iv_set_union(y, x),
+    iv_pairs(c(1, 3), c(NA, NA))
+  )
+})
+
+test_that("union is generic over container", {
+  x <- nested_integer_iv_pairs(c(1, 3))
+  y <- nested_integer_iv_pairs(c(2, 5))
+  expect_identical(iv_set_union(x, y), nested_integer_iv_pairs(c(1, 5)))
+})
+
+# ------------------------------------------------------------------------------
+# iv_set_intersect()
+
+test_that("set intersect doesn't keep abutting", {
+  x <- iv(start = c(1L, 2L), end = c(2L, 3L))
+
+  expect_identical(
+    iv_set_intersect(x, x),
+    iv(start = 1L, end = 3L)
+  )
+})
+
+test_that("intersect works", {
+  x <- iv(start = c(1L, 6L), end = c(4L, 8L))
+  y <- iv(start = 2L, end = 3L)
+  z <- iv(start = 3L, end = 7L)
+
+  expect_identical(
+    iv_set_intersect(x, y),
+    iv(start = 2L, end = 3L)
+  )
+  expect_identical(
+    iv_set_intersect(x, z),
+    iv(start = c(3L, 6L), end = c(4L, 7L))
+  )
+})
+
+test_that("intersect works with size zero inputs", {
+  x <- iv(start = integer(), end = integer())
+  expect_identical(iv_set_intersect(x, x), x)
+
+  y <- iv(start = 1L, end = 2L)
+  expect_identical(iv_set_intersect(x, y), x)
+  expect_identical(iv_set_intersect(y, x), x)
+})
+
+test_that("intersect keeps a single missing interval", {
+  x <- iv(c(NA, 0, NA), c(NA, 2, NA))
+  y <- iv(c(1, NA), c(4, NA))
+
+  expect_identical(
+    iv_set_intersect(x, y),
+    iv_pairs(c(1, 2), c(NA, NA))
+  )
+  expect_identical(
+    iv_set_intersect(y, x),
+    iv_pairs(c(1, 2), c(NA, NA))
+  )
+})
+
+test_that("intersect works when all of one input is missing", {
+  x <- iv(c(NA, NA), c(NA, NA))
+  y <- iv(c(NA, 0, NA), c(NA, 2, NA))
+  z <- iv(0, 2)
+
+  expect_identical(
+    iv_set_intersect(x, y),
+    iv(NA_real_, NA_real_)
+  )
+  expect_identical(
+    iv_set_intersect(y, x),
+    iv(NA_real_, NA_real_)
+  )
+
+  expect_identical(
+    iv_set_intersect(x, z),
+    iv(double(), double())
+  )
+  expect_identical(
+    iv_set_intersect(z, x),
+    iv(double(), double())
+  )
+})
+
+test_that("takes ptype on early exits", {
+  x <- iv(integer(), integer())
+  y <- iv(c(1L, 3L), c(3L, 4L))
+
+  expect_identical(iv_set_intersect(x, y), x)
+  expect_identical(iv_set_intersect(y, x), x)
+
+  z <- iv(NA_integer_, NA_integer_)
+
+  expect_identical(iv_set_intersect(z, y), x)
+  expect_identical(iv_set_intersect(y, z), x)
+})
+
+test_that("intersect is generic over container", {
+  x <- nested_integer_iv_pairs(c(1, 3))
+  y <- nested_integer_iv_pairs(c(2, 3))
+  expect_identical(iv_set_intersect(x, y), nested_integer_iv_pairs(c(2, 3)))
+})
