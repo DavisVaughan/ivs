59d6c10540fde8ebc117096fbf79199e955c12b7
DavisVaughan <davis@rstudio.com>
2022-01-26 17:03:17 -0500

    Move from parallel->pairwise for set operations

--- R/set-pairwise.R
+++ R/set-pairwise.R
@@ -1,32 +1,31 @@
-#' Parallel set operations
+#' Pairwise set operations
 #'
 #' @description
-#' This family of functions performs set operations on two ivs _in parallel_.
-#' Parallel in this context does not refer to multi-CPU processing, but instead
-#' refers to the fact that the i-th interval of `x` is going to be compared
-#' against the i-th interval of `y`. This is in contrast to their non-parallel
-#' counterparts (like [iv_union()]), which operate on the whole sets of `x` and
-#' `y` at once.
+#' This family of functions performs _pairwise_ set operations on two ivs.
+#' Pairwise refers to the fact that the i-th interval of `x` is going to be
+#' compared against the i-th interval of `y`. This is in contrast to their
+#' set-like counterparts (like [iv_union()]), which operate on the whole sets of
+#' `x` and `y` at once.
 #'
-#' The descriptions of these operations are the same as their non-parallel
+#' The descriptions of these operations are the same as their set-like
 #' counterparts, but the ones here also have a number of restrictions due to
 #' the fact that each must return an output that is the same size as its inputs:
 #'
-#' - For `iv_parallel_complement()`, `x[i]` and `y[i]` can't overlap or abut,
+#' - For `iv_pairwise_complement()`, `x[i]` and `y[i]` can't overlap or abut,
 #'   as this would generate an empty complement.
 #'
-#' - For `iv_parallel_union()`, `x[i]` and `y[i]` can't be separated by a gap.
-#'   Use `iv_parallel_span()` if you want to force gaps to be filled anyways.
+#' - For `iv_pairwise_union()`, `x[i]` and `y[i]` can't be separated by a gap.
+#'   Use `iv_pairwise_span()` if you want to force gaps to be filled anyways.
 #'
-#' - For `iv_parallel_intersect()`, `x[i]` and `y[i]` must overlap, otherwise
+#' - For `iv_pairwise_intersect()`, `x[i]` and `y[i]` must overlap, otherwise
 #'   an empty interval would be generated.
 #'
-#' - For `iv_parallel_difference()`, `x[i]` can't be completely contained
+#' - For `iv_pairwise_difference()`, `x[i]` can't be completely contained
 #'   within `y[i]`, as that would generate an empty interval. Additionally,
 #'   `y[i]` can't be completely contained within `x[i]`, as that would result
 #'   in two distinct intervals for a single observation.
 #'
-#' - For `iv_parallel_symmetric_difference()`, `x[i]` and `y[i]` must share
+#' - For `iv_pairwise_symmetric_difference()`, `x[i]` and `y[i]` must share
 #'   exactly one endpoint, otherwise an empty interval or two distinct intervals
 #'   would be generated.
 #'
@@ -41,40 +40,40 @@
 #' @seealso The set-like versions of these functions, such as
 #' [iv_union()].
 #'
-#' @name iv-set-parallel
+#' @name iv-set-pairwise
 #'
 #' @examples
 #' x <- iv_pairs(c(1, 3), c(6, 8))
 #' y <- iv_pairs(c(5, 7), c(2, 3))
 #'
-#' iv_parallel_complement(x, y)
+#' iv_pairwise_complement(x, y)
 #'
 #' z <- iv_pairs(c(2, 5), c(4, 7))
 #'
-#' iv_parallel_union(x, z)
+#' iv_pairwise_union(x, z)
 #'
 #' # Can't take the union when there are gaps
-#' try(iv_parallel_union(x, y))
+#' try(iv_pairwise_union(x, y))
 #'
-#' # But you can force a union across gaps with `iv_parallel_span()`
-#' iv_parallel_span(x, y)
+#' # But you can force a union across gaps with `iv_pairwise_span()`
+#' iv_pairwise_span(x, y)
 #'
-#' iv_parallel_intersect(x, z)
+#' iv_pairwise_intersect(x, z)
 #'
 #' # Can't take an intersection of non-overlapping intervals
-#' try(iv_parallel_intersect(x, y))
+#' try(iv_pairwise_intersect(x, y))
 #'
-#' iv_parallel_difference(x, z)
+#' iv_pairwise_difference(x, z)
 #'
-#' # The parallel symmetric difference function is fairly strict,
+#' # The pairwise symmetric difference function is fairly strict,
 #' # and is only well defined when exactly one of the interval endpoints match
 #' w <- iv_pairs(c(1, 6), c(7, 8))
-#' iv_parallel_symmetric_difference(x, w)
+#' iv_pairwise_symmetric_difference(x, w)
 NULL
 
-#' @rdname iv-set-parallel
+#' @rdname iv-set-pairwise
 #' @export
-iv_parallel_complement <- function(x, y) {
+iv_pairwise_complement <- function(x, y) {
   args <- list(x = x, y = y)
   args <- vec_cast_common(!!!args)
   args <- vec_recycle_common(!!!args)
@@ -90,8 +89,8 @@ iv_parallel_complement <- function(x, y) {
   x_end <- field_end(x_proxy)
   y_end <- field_end(y_proxy)
 
-  end <- vec_parallel_max(x_start, y_start)
-  start <- vec_parallel_min(x_end, y_end)
+  end <- vec_pairwise_max(x_start, y_start)
+  start <- vec_pairwise_min(x_end, y_end)
 
   overlaps_or_abuts <- start >= end
   if (any(overlaps_or_abuts, na.rm = TRUE)) {
@@ -109,9 +108,9 @@ iv_parallel_complement <- function(x, y) {
   out
 }
 
-#' @rdname iv-set-parallel
+#' @rdname iv-set-pairwise
 #' @export
-iv_parallel_union <- function(x, y) {
+iv_pairwise_union <- function(x, y) {
   args <- list(x = x, y = y)
   args <- vec_cast_common(!!!args)
   args <- vec_recycle_common(!!!args)
@@ -130,8 +129,8 @@ iv_parallel_union <- function(x, y) {
   compare_start <- vec_compare(x_start, y_start)
   compare_end <- vec_compare(x_end, y_end)
 
-  max_start <- vec_parallel_max(x_start, y_start, compare = compare_start)
-  min_end <- vec_parallel_min(x_end, y_end, compare = compare_end)
+  max_start <- vec_pairwise_max(x_start, y_start, compare = compare_start)
+  min_end <- vec_pairwise_min(x_end, y_end, compare = compare_end)
 
   has_gap <- vec_compare(max_start, min_end) == 1L
   if (any(has_gap, na.rm = TRUE)) {
@@ -140,12 +139,12 @@ iv_parallel_union <- function(x, y) {
     abort(c(
       "Can't take the union of intervals containing a gap.",
       i = glue("Location {loc} contains a gap."),
-      i = "Use `iv_parallel_span()` to combine across gaps."
+      i = "Use `iv_pairwise_span()` to combine across gaps."
     ))
   }
 
-  start <- vec_parallel_min(x_start, y_start, compare = compare_start)
-  end <- vec_parallel_max(x_end, y_end, compare = compare_end)
+  start <- vec_pairwise_min(x_start, y_start, compare = compare_start)
+  end <- vec_pairwise_max(x_end, y_end, compare = compare_end)
 
   out <- new_iv(start, end)
   out <- iv_restore(out, x)
@@ -153,9 +152,9 @@ iv_parallel_union <- function(x, y) {
   out
 }
 
-#' @rdname iv-set-parallel
+#' @rdname iv-set-pairwise
 #' @export
-iv_parallel_span <- function(x, y) {
+iv_pairwise_span <- function(x, y) {
   args <- list(x = x, y = y)
   args <- vec_cast_common(!!!args)
   args <- vec_recycle_common(!!!args)
@@ -171,8 +170,8 @@ iv_parallel_span <- function(x, y) {
   x_end <- field_end(x_proxy)
   y_end <- field_end(y_proxy)
 
-  start <- vec_parallel_min(x_start, y_start)
-  end <- vec_parallel_max(x_end, y_end)
+  start <- vec_pairwise_min(x_start, y_start)
+  end <- vec_pairwise_max(x_end, y_end)
 
   out <- new_iv(start, end)
   out <- iv_restore(out, x)
@@ -180,9 +179,9 @@ iv_parallel_span <- function(x, y) {
   out
 }
 
-#' @rdname iv-set-parallel
+#' @rdname iv-set-pairwise
 #' @export
-iv_parallel_intersect <- function(x, y) {
+iv_pairwise_intersect <- function(x, y) {
   args <- list(x = x, y = y)
   args <- vec_cast_common(!!!args)
   args <- vec_recycle_common(!!!args)
@@ -198,8 +197,8 @@ iv_parallel_intersect <- function(x, y) {
   x_end <- field_end(x_proxy)
   y_end <- field_end(y_proxy)
 
-  start <- vec_parallel_max(x_start, y_start)
-  end <- vec_parallel_min(x_end, y_end)
+  start <- vec_pairwise_max(x_start, y_start)
+  end <- vec_pairwise_min(x_end, y_end)
 
   non_overlapping <- vec_compare(start, end) >= 0L
   if (any(non_overlapping, na.rm = TRUE)) {
@@ -218,9 +217,9 @@ iv_parallel_intersect <- function(x, y) {
   out
 }
 
-#' @rdname iv-set-parallel
+#' @rdname iv-set-pairwise
 #' @export
-iv_parallel_difference <- function(x, y) {
+iv_pairwise_difference <- function(x, y) {
   args <- list(x = x, y = y)
   args <- vec_recycle_common(!!!args)
   args <- vec_cast_common(!!!args)
@@ -264,8 +263,8 @@ iv_parallel_difference <- function(x, y) {
   start <- x_start
   end <- x_end
 
-  max_start <- vec_parallel_max(x_start, y_start, compare = compare_start)
-  min_end <- vec_parallel_min(x_end, y_end, compare = compare_end)
+  max_start <- vec_pairwise_max(x_start, y_start, compare = compare_start)
+  min_end <- vec_pairwise_min(x_end, y_end, compare = compare_end)
 
   update <- vec_compare(max_start, min_end) < 0L
   direction <- vec_equal(min_end, x_end)
@@ -293,9 +292,9 @@ iv_parallel_difference <- function(x, y) {
   out
 }
 
-#' @rdname iv-set-parallel
+#' @rdname iv-set-pairwise
 #' @export
-iv_parallel_symmetric_difference <- function(x, y) {
+iv_pairwise_symmetric_difference <- function(x, y) {
   args <- list(x = x, y = y)
   args <- vec_recycle_common(!!!args)
   args <- vec_cast_common(!!!args)
@@ -345,12 +344,12 @@ iv_parallel_symmetric_difference <- function(x, y) {
     equal_start <- which(equal_start)
     compare_end <- vec_slice(compare_end, equal_start)
 
-    new_start <- vec_parallel_min(
+    new_start <- vec_pairwise_min(
       vec_slice(x_end, equal_start),
       vec_slice(y_end, equal_start),
       compare = compare_end
     )
-    new_end <- vec_parallel_max(
+    new_end <- vec_pairwise_max(
       vec_slice(x_end, equal_start),
       vec_slice(y_end, equal_start),
       compare = compare_end
@@ -364,12 +363,12 @@ iv_parallel_symmetric_difference <- function(x, y) {
     equal_end <- which(equal_end)
     compare_start <- vec_slice(compare_start, equal_end)
 
-    new_start <- vec_parallel_min(
+    new_start <- vec_pairwise_min(
       vec_slice(x_start, equal_end),
       vec_slice(y_start, equal_end),
       compare = compare_start
     )
-    new_end <- vec_parallel_max(
+    new_end <- vec_pairwise_max(
       vec_slice(x_start, equal_end),
       vec_slice(y_start, equal_end),
       compare = compare_start
@@ -387,13 +386,13 @@ iv_parallel_symmetric_difference <- function(x, y) {
 
 # ------------------------------------------------------------------------------
 
-vec_parallel_min <- function(x, y, ..., compare = NULL) {
-  vec_parallel_summary(x, y, type = "min", compare = compare)
+vec_pairwise_min <- function(x, y, ..., compare = NULL) {
+  vec_pairwise_summary(x, y, type = "min", compare = compare)
 }
-vec_parallel_max <- function(x, y, ..., compare = NULL) {
-  vec_parallel_summary(x, y, type = "max", compare = compare)
+vec_pairwise_max <- function(x, y, ..., compare = NULL) {
+  vec_pairwise_summary(x, y, type = "max", compare = compare)
 }
-vec_parallel_summary <- function(x, y, type, ..., compare = NULL) {
+vec_pairwise_summary <- function(x, y, type, ..., compare = NULL) {
   args <- list(x = x, y = y)
   args <- vec_cast_common(!!!args)
   args <- vec_recycle_common(!!!args)
