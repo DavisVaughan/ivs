9bad97d86dc2697dc3573e6aa43fcb8fff14eee7
DavisVaughan <davis@rstudio.com>
2022-04-01 14:43:11 -0400

    Remove unused helper

--- R/utils.R
+++ R/utils.R
@@ -4,10 +4,6 @@ vec_paste0 <- function(...) {
   exec(paste0, !!!args)
 }
 
-stop_iv <- function(message = NULL, class = NULL, ..., call = caller_env()) {
-  abort(message, class = c(class, "iv_error"), ..., call = call)
-}
-
 err_locs <- function(x) {
   if (!is.integer(x)) {
     abort("`x` must be an integer vector of locations.", .internal = TRUE)
